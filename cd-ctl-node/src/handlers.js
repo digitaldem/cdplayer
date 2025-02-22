3a8720fc028678d3e8b25ad300121b2881707c54
Dave Murdoch <dave@digitaldementia.com>
2025-02-21 18:51:06 -0600

    fix custom b64 encode

--- cd-ctl-node/src/handlers.js
+++ cd-ctl-node/src/handlers.js
@@ -34,9 +34,9 @@ const info = async (req, res) => {
     discId = crypto.createHash('sha1')
                    .update(toc.join(''))
                    .digest('base64')
-                   .replace(/\+/g, '.')
-                   .replace(/\//g, '_')
-                   .replace(/=/g, '');
+                   .replace(/\+/g, '_')
+                   .replace(/\//g, '.')
+                   .replace(/=/g, '-');
 
     const response = await axios.get(`${MB_URL}/${discId}?fmt=json`, MB_HEADERS);
     const metadata = response.data;
