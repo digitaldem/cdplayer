599fa359a913ee2d160e6c762fa60a1a74471dd4
Dave Murdoch <dave@digitaldementia.com>
2025-02-23 12:03:01 -0600

    Update index.js

--- cd-ctl-node/src/index.js
+++ cd-ctl-node/src/index.js
@@ -6,14 +6,15 @@ const handlers = require('./handlers');
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type'] }));
-app.get('/status', handlers.status);
-app.get('/info', handlers.info);
+app.post('/insert', handlers.insert);
+app.post('/eject', handlers.eject);
 app.post('/play', handlers.play);
 app.post('/pause', handlers.pause);
 app.post('/stop', handlers.stop);
 app.post('/next', handlers.next);
 app.post('/previous', handlers.previous);
-app.post('/eject', handlers.eject);
+app.get('/info', handlers.info);
+app.get('/status', handlers.status);
 app.listen(PORT, HOST, () => {
   console.log(`CD player API running on http://${HOST}:${PORT}`);
 });
