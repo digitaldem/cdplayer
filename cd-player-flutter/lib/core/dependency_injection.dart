7439ee268a095bda43354ff7e609fc06be8884e6
Dave Murdoch <dave@digitaldementia.com>
2025-12-13 15:07:31 -0600

    Update dependency_injection.dart

--- cd-player-flutter/lib/core/dependency_injection.dart
+++ cd-player-flutter/lib/core/dependency_injection.dart
@@ -6,12 +6,13 @@ import '../networking/web_socket_client.dart';
 import '../presentation/providers/player_provider.dart';
 import './constants.dart';
 import './display_sleep_manager.dart';
+import './simple_logger.dart';
 
 class DependencyInjection {
   static final getIt = GetIt.instance;
   static Future<void> init() async {
     // Logging
-    getIt.registerSingleton<Logger>(Logger(printer: SimplePrinter(printTime: true, colors: true), filter: _CustomLogFilter()));
+    getIt.registerSingleton<Logger>(SimpleLogger());
 
     // Display Sleep
     getIt.registerSingleton<DisplaySleepManager>(DisplaySleepManager(timeout: const Duration(minutes: 2)));
