4d41338ba1e7924e05ed5b9e0d0637beb6702eaf
Dave Murdoch <dave@digitaldementia.com>
2026-03-21 13:44:09 -0500

    Add MPV_SOCKET constant for socket path

--- cd-ctl-node/src/constants.js
+++ cd-ctl-node/src/constants.js
@@ -3,6 +3,7 @@ const PORT = 8080;
 
 const CD_DEVICE = '/dev/cdrom';
 const AUDIO_DEVICE = 'pipewire/alsa_output.usb-BurrBrown_from_Texas_Instruments_USB_AUDIO_DAC-00.analog-stereo';
+const MPV_SOCKET = '/tmp/mpv-cd-socket';
 const SECTOR_OFFSET = 150;
 const MB_URL = 'https://musicbrainz.org/ws/2/discid';
 const MB_HEADERS = { 'User-Agent': 'CDPlayer/1.0.0 (dave@digitaldementia.com)' };
@@ -12,6 +13,7 @@ module.exports = {
   PORT,
   CD_DEVICE,
   AUDIO_DEVICE,
+  MPV_SOCKET,
   SECTOR_OFFSET,
   MB_URL,
   MB_HEADERS,
